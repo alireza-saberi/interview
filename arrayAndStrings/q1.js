5092d0ee7f7060b8958df7e7bfe9824098739bfe
Ali <saberi.ar.eng@gmail.com>
2015-10-13 14:31:32 -0400

    feat: array and strings q1
        first common questions in interviews if something is unique or not

---
+++ arrayAndStrings/q1.js
@@ -0,0 +1,28 @@
+// implement an algorithm that determine if a string has all unique characters. What  if you cannot use additional data structure
+// Ask if the string is in ASCII or in Unicode
+
+// optimization : return false in the length of string is greater than length of available characters!
+
+function solution1(A){
+	var counter = {},
+		elm;
+
+	if (A.length <= 256){
+		
+		for (var i = 0; i< A.length ;i++){
+			elm = A[i];
+			if(!counter[elm]){
+				counter[elm] = 1;
+			}
+			else{
+				return false;
+			}
+		};
+	}else{
+		return false;
+	}
+	return true;
+}
+
+A = [4, 3, 5, 6];
+console.log(solution1(A));
