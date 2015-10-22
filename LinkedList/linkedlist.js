d41d75aee896d992977daca56cc54f3a983380f6
alireza-saberi <saberi.ar.eng@gmail.com>
2015-10-22 17:15:58 -0400

    feat: linked list functions

--- LinkedList/linkedlist.js
+++ LinkedList/linkedlist.js
@@ -1,11 +1,13 @@
 "use strict"
-// LinkedList is a sequence of objects called nodes, each node has data and address of next node.
+// LinkedList is a sequence of objects called nodes, each node has data and address(successor) of next node.
 // JS has garbage collector; if you remove B from A->B->C since there is no refernce to B, the JS garbage collector will take care of it
 function List(){
 	this.start = null;
 	this.end = null;
 	var makeNode = function(){
-		return {data:null, next:null};
+		return {
+				data:null,
+				next:null};
 	};
 	this.add = function(data){
 		if(this.start === null){
@@ -39,6 +41,29 @@ function List(){
  			previous = current; 
  			current = current.next; 
  			}
+	};
+	this.inserAfter = function(searchData, data){
+		var current = this.start;
+		while( current !== null){
+			if( current.data === searchData){
+				var temp = makeNode();
+				temp.data = searchData;
+				temp.next = current.next;
+				if( current == this.end) this.end = temp;
+				current.next = temp;
+				return;
+			}
+		current = current.next;
+		}
+	};
+	this.item = function(i){
+		var current = this.start;
+		while( current !== null){
+			i--;
+			if(i === 0) return current;
+			current = current.next;
+		}
+		return null;
 	};
 }
 
